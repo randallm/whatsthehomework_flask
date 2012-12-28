dcf9f681036b4ac61832f7a661173e8f36f532b4
Randall Ma <randall@randallma.com>
2012-12-28 09:50:19 -0800

    itsdangerous client-side session implementatino

--- wth/__init__.py
+++ wth/__init__.py
@@ -2,9 +2,15 @@ from flask import Flask
 from flask_peewee.db import Database
 from peewee import *
 from flask_peewee.admin import Admin
+from datetime import timedelta
+
 
 app = Flask(__name__)
 app.config.from_object('wth.config')
+
+app.session_interface = ItsdangerousSessionInterface()
+# session.permanent = True (already set in auth.py)
+app.permanent_session_lifetime = timedelta(minutes=9999999)
 
 db = Database(app)
 
